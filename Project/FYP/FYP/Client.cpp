106b400f1f0b1287de1853ace8a2b3f3636b1b74
JonnyIrl <swtorjonny@hotmail.com>
2016-04-14 15:21:30 +0100

    new positions for the red and green ready rects
    Looks much better and clearer

--- Project/FYP/FYP/Client.cpp
+++ Project/FYP/FYP/Client.cpp
@@ -13,42 +13,42 @@ void Client::AssignPosition(int count)
 {
 	if (count == 1)
 	{
-		m_readySquare.setPosition(sf::Vector2f(850, 195 + (20 * count)));
+		m_readySquare.setPosition(sf::Vector2f(850, 195 + (18 * count)));
 	}
 
 	if (count == 2)
 	{
-		m_readySquare.setPosition(sf::Vector2f(850, 195 + (20 * count)));
+		m_readySquare.setPosition(sf::Vector2f(850, 195 + (28 * count)));
 	}
 
 	if (count == 3)
 	{
-		m_readySquare.setPosition(sf::Vector2f(850, 195 + (20 * count)));
+		m_readySquare.setPosition(sf::Vector2f(850, 195 + (28 * count)));
 	}
 
 	if (count == 4)
 	{
-		m_readySquare.setPosition(sf::Vector2f(850, 195 + (20 * count)));
+		m_readySquare.setPosition(sf::Vector2f(850, 195 + (28 * count)));
 	}
 
 	if (count == 5)
 	{
-		m_readySquare.setPosition(sf::Vector2f(850, 195 + (20 * count)));
+		m_readySquare.setPosition(sf::Vector2f(850, 195 + (28 * count)));
 	}
 
 	if (count == 6)
 	{
-		m_readySquare.setPosition(sf::Vector2f(850, 195 + (20 * count)));
+		m_readySquare.setPosition(sf::Vector2f(850, 195 + (28 * count)));
 	}
 
 	if (count == 7)
 	{
-		m_readySquare.setPosition(sf::Vector2f(850, 195 + (20 * count)));
+		m_readySquare.setPosition(sf::Vector2f(850, 195 + (28 * count)));
 	}
 
 	if (count == 8)
 	{
-		m_readySquare.setPosition(sf::Vector2f(850, 195 + (20 * count)));
+		m_readySquare.setPosition(sf::Vector2f(850, 195 + (28 * count)));
 	}
 }
 
